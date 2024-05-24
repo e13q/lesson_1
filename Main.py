22797fec49db5e13c37bfd9ad6e2edb1b1f41b79
e13q <110967581+e13q@users.noreply.github.com>
2024-05-25 01:34:00 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,18 +1,21 @@
-import requests  
+import requests
+
 
 if __name__ == '__main__':
     payload = {
                 'lang': 'ru',
                 'T': '',
                 'q': '',
-                'M': '',    
-                'n': '', 
-            }  
+                'M': '',
+                'n': '',
+    }
     for point in ['Sheremetyevo', 'London', 'Cherepovec']:
-        response = requests.get(url = f'https://wttr.in/{point}', params=payload) 
+        url = f'https://wttr.in/{point}'
+        response = requests.get(url, params=payload)
         try:
             response.raise_for_status()
-            if response.ok:
-                print(response.text)
+            print(response.text)
         except requests.exceptions.HTTPError:
-            print(f'Response for {point} failed. Status code: {response.status_code}') 
+            print(f'Response for {point} failed')
+            print(f'Status code: {response.status_code}')
+
